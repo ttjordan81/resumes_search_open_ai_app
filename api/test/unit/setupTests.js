707db55ad9d3599e894589fbe7df8c116d92aca1
Timothy Jordan <tjordan@ahpnet.com>
2024-03-09 19:41:51 -0500

    fix deployment

--- api/test/unit/setupTests.js
+++
@@ -1,8 +0,0 @@
-global.log = { 
-    info: jest.fn((msg) => { 
-        //console.log("log.info() message:", msg)
-    }),
-    error: jest.fn((msg) => { 
-        // console.log("log.error() message:", msg)
-    })
-};
